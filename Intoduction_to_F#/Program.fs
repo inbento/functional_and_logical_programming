2235259049a273c7bc26e533072bc068fd202fa1
inbento <mikov.ns.04@gmail.com>
2025-03-27 22:09:52 +0300

    Lab1 task1-2(Hello World and Quadratic function)

---
+++ Intoduction_to_F#/Program.fs
@@ -0,0 +1,30 @@
+﻿open System
+
+type SolveResult =
+    None
+    | Linear of float
+    | Quadratic of float * float
+
+let solve a b c = 
+        let D = b * b - 4.0 * a * c
+        if a = 0.0 then
+            if b = 0.0 then None
+            else Linear(-c / b)
+        else
+            if D < 0.0 then None
+            else Quadratic(((-b + sqrt(D)) / (2.0 * a), (-b - sqrt(D)) / (2.0 * a)))
+
+
+
+
+[<EntryPoint>]
+let main argv = 
+
+    Console.WriteLine("Hello World!") //напишет в консоль Hello World
+
+    let a = 2.0
+    let b = -6.0
+    let c = 1.0
+    Console.WriteLine(solve a b c)
+
+    0
