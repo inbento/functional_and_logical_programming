8d615d9b3818deba980609759b07418de0cf40ba
inbento <mikov.ns.04@gmail.com>
2025-03-27 23:06:56 +0300

    Lab1 task3.1 (superposition)

--- Intoduction_to_F#/Program.fs
+++ Intoduction_to_F#/Program.fs
@@ -14,6 +14,14 @@ let solve a b c =
             if D < 0.0 then None
             else Quadratic(((-b + sqrt(D)) / (2.0 * a), (-b - sqrt(D)) / (2.0 * a)))
 
+let circle_area rad=
+    Math.PI * rad * rad
+  
+let myltiply area h =
+    area * h
+
+let cylinder_area =
+    circle_area >> myltiply
 
 
 
@@ -21,10 +29,15 @@ let solve a b c =
 let main argv = 
 
     Console.WriteLine("Hello World!") //напишет в консоль Hello World
-
     let a = 2.0
     let b = -6.0
     let c = 1.0
     Console.WriteLine(solve a b c)
+
+    Console.WriteLine("Введите радиус: ")
+    let rad = Console.ReadLine() |> float
+    Console.WriteLine("Введите высоту цилиндра: ")
+    let h = Console.ReadLine() |> float
+    Console.WriteLine($"Площадь цилиндра: {cylinder_area rad h}")
 
     0
