b3a90237791addcfbcb4abf15e3c379043976515
inbento <mikov.ns.04@gmail.com>
2025-04-17 21:10:25 +0300

    lab3.1.8-9 added a class in the form of an algebraic type

--- OOP/Program.fs
+++ OOP/Program.fs
@@ -1,67 +1,44 @@
 ﻿open System
 
-type IPrint = interface
-    abstract member Print: unit -> unit
-    end
+type GeometricFigure =
+    | Rectangle of width: float * height: float
+    | Square of side: float
+    | Circle of radius: float
 
-[<AbstractClass>]
-type GeometricFigure() =
-    abstract member CalculateArea: unit -> float
-    override this.ToString() = "Геометрическая фигура"
+let calculateArea figure =
+    match figure with
+    | Rectangle (w, h) -> w * h
+    | Square s -> s * s
+    | Circle r -> Math.PI * r * r
 
-type Rectangle(width: float, height: float) =
-    inherit GeometricFigure()
-    interface IPrint with
-        member this.Print() = Console.WriteLine(this.ToString())
+let figureToString figure =
+    match figure with
+    | Rectangle (w, h) -> 
+        String.Format("Прямоугольник [ширина={0:F2}, высота={1:F2}, площадь={2:F2}]", w, h, calculateArea figure)
+    | Square s -> 
+        String.Format("Квадрат [сторона={0:F2}, площадь={1:F2}]", s, calculateArea figure)
+    | Circle r -> 
+        String.Format("Круг [радиус={0:F2}, площадь={1:F2}]", r, calculateArea figure)
 
-    member val Width = width with get, set
-    member val Height = height with get, set
-
-    override this.CalculateArea() = this.Width * this.Height
-    override this.ToString() =
-        String.Format("Прямоугольник [ширина={0:F2}, высота={1:F2}, площадь={2:F2}]", 
-                     this.Width, this.Height, this.CalculateArea())
-    new() = Rectangle(0.0, 0.0)
-
-type Square(side: float) =
-    inherit Rectangle(side, side)
-    interface IPrint with
-        member this.Print() = Console.WriteLine(this.ToString())
-
-    member this.Side
-        with get() = this.Width
-        and set(value) = 
-            this.Width <- value
-            this.Height <- value
-    override this.ToString() =
-        String.Format("Квадрат [сторона={0:F2}, площадь={1:F2}]", 
-                     this.Side, this.CalculateArea())
-    new() = Square(0.0)
-
-type Circle(radius: float) =
-    inherit GeometricFigure()
-    interface IPrint with
-        member this.Print() = Console.WriteLine(this.ToString())
-        
-    member val Radius = radius with get, set
-    
-    override this.CalculateArea() = Math.PI * this.Radius * this.Radius
-    override this.ToString() =
-        String.Format("Круг [радиус={0:F2}, площадь={1:F2}]", 
-                     this.Radius, this.CalculateArea())
-    new() = Circle(0.0)
-
+let printFigure figure =
+    Console.WriteLine(figureToString figure)
 
 [<EntryPoint>]
 let main argv =
+    let rect = Rectangle(9.0, 2.0)
+    let square = Square(12.0)
+    let circle = Circle(8.0)
 
-    let rect = Rectangle(5.0, 3.0)
-    (rect :> IPrint).Print()
+    Console.WriteLine("Площадь прямоугольника: {0:F2}", calculateArea rect)
+    Console.WriteLine("Площадь квадрата: {0:F2}", calculateArea square)
+    Console.WriteLine("Площадь круга: {0:F2}", calculateArea circle)
 
-    let square = Square(4.0)
-    (square :> IPrint).Print()
+    printFigure rect
+    printFigure square
+    printFigure circle
 
-    let circle = Circle(3.0)
-    (circle :> IPrint).Print()
-    
+    let figures = [rect; square; circle]
+    Console.WriteLine("\nПечать всех фигур:")
+    figures |> List.iter printFigure
+
     0
