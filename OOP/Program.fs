0ed5ed410940310a8988b0315f34cfbea5fa81e8
inbento <mikov.ns.04@gmail.com>
2025-04-17 17:48:27 +0300

    lab3.1.6 override method ToString

--- OOP/Program.fs
+++ OOP/Program.fs
@@ -3,7 +3,7 @@
 [<AbstractClass>]
 type GeometricFigure() =
     abstract member CalculateArea: unit -> float
-
+    override this.ToString() = "Геометрическая фигура"
 
 type Rectangle(width: float, height: float) =
     inherit GeometricFigure()
@@ -12,7 +12,9 @@ type Rectangle(width: float, height: float) =
     member val Height = height with get, set
 
     override this.CalculateArea() = this.Width * this.Height
-
+    override this.ToString() =
+        String.Format("Прямоугольник [ширина={0:F2}, высота={1:F2}, площадь={2:F2}]", 
+                     this.Width, this.Height, this.CalculateArea())
     new() = Rectangle(0.0, 0.0)
 
 type Square(side: float) =
@@ -23,7 +25,9 @@ type Square(side: float) =
         and set(value) = 
             this.Width <- value
             this.Height <- value
-
+    override this.ToString() =
+        String.Format("Квадрат [сторона={0:F2}, площадь={1:F2}]", 
+                     this.Side, this.CalculateArea())
     new() = Square(0.0)
 
 type Circle(radius: float) =
@@ -32,33 +36,24 @@ type Circle(radius: float) =
     member val Radius = radius with get, set
     
     override this.CalculateArea() = Math.PI * this.Radius * this.Radius
-    
+    override this.ToString() =
+        String.Format("Круг [радиус={0:F2}, площадь={1:F2}]", 
+                     this.Radius, this.CalculateArea())
     new() = Circle(0.0)
 
-let printFigureInfo (figure: GeometricFigure) =
-    match figure with
-    | :? Square as sq -> 
-        Console.WriteLine("Квадрат: сторона = {0:F2}, площадь = {1:F2}", 
-                         sq.Side, sq.CalculateArea())
-    | :? Rectangle as rect -> 
-        Console.WriteLine("Прямоугольник: ширина = {0:F2}, высота = {1:F2}, площадь = {2:F2}", 
-                          rect.Width, rect.Height, rect.CalculateArea())
-    | :? Circle as circle -> 
-        Console.WriteLine("Круг: радиус = {0:F2}, площадь = {1:F2}", 
-                         circle.Radius, circle.CalculateArea())
-    | _ -> Console.WriteLine("Неизвестная фигура")
 
 [<EntryPoint>]
 let main argv =
 
     let rect = Rectangle(5.0, 3.0)
-    printFigureInfo rect
+    Console.WriteLine(rect.ToString())
 
     let square = Square(4.0)
-    printFigureInfo square
-    
+    Console.WriteLine(square.ToString())
+
     let circle = Circle(3.0)
-    printFigureInfo circle
+    Console.WriteLine(circle.ToString())
+
 
     
     0
