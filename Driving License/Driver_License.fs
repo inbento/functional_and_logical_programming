ebca7e6dbb6cd6575c83d615c178c19fa1d680c0
inbento <mikov.ns.04@gmail.com>
2025-04-19 00:25:28 +0300

    lab3.5 added class Driver_License and tests

---
+++ Driving License/Driver_License.fs
@@ -0,0 +1,86 @@
+﻿module Driver_License
+open System
+open System.Text.RegularExpressions
+
+type DriverLicense( lastName: string, firstName: string, patronymic: string, birthDateAndPlace: string,
+    issueDate: DateTime, expiryDate: DateTime, issuingAuthority: string, licenseNumber: string,
+    address: string, categories: string) =
+    
+    let validate (pattern: string) (errorMessage: string) (value: string) =
+        if Regex.IsMatch(value, pattern) then value
+        else failwith errorMessage
+    
+    let validatedLastName = 
+        validate @"^[А-ЯЁ][а-яё]+(-[А-ЯЁ][а-яё]+)?$" 
+            "Фамилия должна начинаться с заглавной буквы и содержать только буквы и дефисы" lastName
+    
+    let validatedFirstName = 
+        validate @"^[А-ЯЁ][а-яё]+$" 
+            "Имя должно начинаться с заглавной буквы и содержать только буквы" firstName
+    
+    let validatedPatronymic = 
+        validate @"^[А-ЯЁ][а-яё]*$" 
+            "Отчество должно начинаться с заглавной буквы и содержать только буквы" patronymic
+    
+    let validatedBirthDateAndPlace = 
+        validate @"^\d{2}\.\d{2}\.\d{4}\s[А-ЯЁа-яё\s\-.,]+$" 
+            "Дата и место рождения должны быть в формате 'дд.мм.гггг Место рождения'" birthDateAndPlace
+    
+    let validatedIssuingAuthority = 
+        validate @"^ГИБДД\s\d{4}$" 
+            "Подразделение ГИБДД должно быть в формате 'ГИБДД 1234'" issuingAuthority
+    
+    let validatedLicenseNumber = 
+        validate @"^\d{2}\s\d{2}\s\d{6}$" 
+            "Номер удостоверения должен быть в формате 'xx xx xxxxxx'" licenseNumber
+    
+    let validatedCategories = 
+        validate @"^[A-ZА-ЯЁ]( [A-ZА-ЯЁ])*$" 
+            "Категории должны быть указаны через пробел (пример: A B C D)" categories
+    
+    do
+        if expiryDate <= issueDate then
+            failwith "Дата окончания действия должна быть позже даты выдачи"
+    
+    member _.LastName = validatedLastName
+    member _.FirstName = validatedFirstName
+    member _.Patronymic = validatedPatronymic
+    member _.BirthDateAndPlace = validatedBirthDateAndPlace
+    member _.IssueDate = issueDate
+    member _.ExpiryDate = expiryDate
+    member _.IssuingAuthority = validatedIssuingAuthority
+    member _.LicenseNumber = validatedLicenseNumber
+    member _.Address = address
+    member _.Categories = validatedCategories
+    
+    override this.ToString() =
+        let issueDateStr = this.IssueDate.ToString("dd.MM.yyyy")
+        let expiryDateStr = this.ExpiryDate.ToString("dd.MM.yyyy")
+        
+        String.Join(Environment.NewLine,
+            "Водительское удостоверение",
+            "",
+            $"Фамилия: {this.LastName}",
+            $"Имя: {this.FirstName}",
+            $"Отчество: {this.Patronymic}",
+            $"Дата и место рождения: {this.BirthDateAndPlace}",
+            $"Дата выдачи: {issueDateStr}",
+            $"Дата окончания срока действия: {expiryDateStr}",
+            $"Выдано: {this.IssuingAuthority}",
+            $"Номер удостоверения: {this.LicenseNumber}",
+            $"Место жительства: {this.Address}",
+            $"Категории: {this.Categories}")
+
+    interface IComparable with
+        member this.CompareTo(obj) =
+            match obj with
+            | :? DriverLicense as other -> this.LicenseNumber.CompareTo(other.LicenseNumber)
+            | _ -> -1
+    
+    override this.Equals(obj) =
+        match obj with
+        | :? DriverLicense as other -> this.LicenseNumber = other.LicenseNumber
+        | _ -> false
+    
+    override this.GetHashCode() =
+        hash licenseNumber
---
+++ Driving License/Program.fs
@@ -0,0 +1,43 @@
+﻿open System
+open Driver_License
+
+
+let test() =
+    let license1 = DriverLicense(
+        lastName = "Миков",
+        firstName = "Никита",
+        patronymic = "Сергеевич",
+        birthDateAndPlace = "03.03.2003 г. Москва",
+        issueDate = DateTime(2020, 1, 1),
+        expiryDate = DateTime(2030, 1, 1),
+        issuingAuthority = "ГИБДД 1234",
+        licenseNumber = "12 34 567890",
+        address = "г. Москва, ул. Ленина, д. 1",
+        categories = "B С M")
+    
+    Console.WriteLine(license1)
+
+    let license2 = DriverLicense(
+        lastName = "Бимбим",
+        firstName = "Бамбам",
+        patronymic = "Парампам",
+        birthDateAndPlace = "11.11.2011 г. Воронеж",
+        issueDate = DateTime(2024, 2, 4),
+        expiryDate = DateTime(2034, 2, 4),
+        issuingAuthority = "ГИБДД 5678",
+        licenseNumber = "56 78 901234",
+        address = "г. Воронеж, ул. Ленина, д. 21",
+        categories = "A B C")
+
+    Console.WriteLine(license2)
+
+    Console.WriteLine($"ВУ1 = ВУ2 {license1.Equals(license2)}")
+    Console.WriteLine($"ВУ1 = ВУ1 {license1.Equals(license1)}")
+    Console.WriteLine($"ВУ1 > ВУ2 {license1 > license2}")
+
+[<EntryPoint>]
+let main argv =
+
+    test()
+
+    0
