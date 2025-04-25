46926e33a499aad32135397effb9c890c7d8238e
inbento <mikov.ns.04@gmail.com>
2025-04-26 01:33:59 +0300

    lab8.3 added grand_pa and uncle

--- Prolog/lab8.pl
+++ Prolog/lab8.pl
@@ -3,3 +3,12 @@ father(Y) :- father(X,Y), print(X).
 
 sister(X,Y) :-  woman(X), X \=Y, parent(Z,X), parent(Z,Y).
 sisters(X) :- sister(Y,X), Y \=X, print(Y), nl, !.
+
+grand_pa(X,Y) :- man(X), parent(X,Z), parent(Z,Y).
+grand_pas(X) :- grand_pa(Y,X), print(Y), nl, fail.
+
+grand_pa_and_son(X,Y) :- grand_pa(X,Y); grand_pa(Y,X).
+
+bro_or_sis(X,Y) :- parent(Z, X), parent(Z, Y), X \=Y.
+uncle(X,Y) :- man(X), parent(Z,Y), bro_or_sis(X,Z), X\=Z.
+uncles(X) :- uncle(Y,X), X \= Y, print(Y), nl, fail.
