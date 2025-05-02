ae87cc6f4e0f58540034290a814b69c6a462651e
inbento <mikov.ns.04@gmail.com>
2025-05-02 23:28:25 +0300

    lab9.2 Multiply,Count odd, GCD

--- Prolog/lab9.pl
+++ Prolog/lab9.pl
@@ -1,45 +1,39 @@
-%максимум двух чисел(принимает X,Y в Z хранится максимум)
+%Задание 2 (Вар 7)
 
-%max(X,Y,Z) :- X>Y, Z is X
+proiz_cifr(0,1) :- !.
+proiz_cifr(N,P) :- Cifr is N mod 10, N1 is N div 10, proiz_cifr(N1, P1), P is P1 * Cifr.
 
-max(X,Y,X) :- X>Y, !.
-%max(X,Y,Y). будет жаловатся, что Х не используется, потому сделаем так:
-max(_,Y,Y).
+proiz_cifr_down(N,P) :- proiz_cifr_down(N,1,P).
+proiz_cifr_down(0,Pr,Pr) :- !.
+proiz_cifr_down(N,Cur_pr,Pr) :- Cifr is N mod 10, N1 is N div 10, New_cur_pr is Cur_pr * Cifr,
+ 				proiz_cifr_down(N1,New_cur_pr,Pr).
+
+count_odd_up(0,0) :- !.
+count_odd_up(N,C) :- Cifr is N mod 10, N1 is N div 10, count_odd_up(N1,C1), 
+	((Cifr > 3, 1 is Cifr mod 2) -> C is C1 + 1;
+	C is C1).
+
+count_odd_down(N,C) :- count_odd_down(N,0,C).
+count_odd_down(0,Count,Count) :- !.
+count_odd_down(N,Cur_count,Count) :- Cifr is N mod 10, N1 is N div 10, 	
+				((Cifr > 3, 1 is Cifr mod 2) -> New_cur_count is Cur_count + 1;
+				New_cur_count is Cur_count),
+ 				count_odd_down(N1,New_cur_count,Count).
+
+gcd(0, N, N) :- !.
+gcd(N, 0, N) :- !.
+gcd(N1, N2, Result) :-
+    N1 >= N2 -> Nod is N1 mod N2, gcd(Nod, N2, Result);
+    gcd(N2, N1, Result).
 
 
-%максимум 3-х
-
-max(X,Y,Z,U) :- max(X,Y,V), max(V,Z,U).
-
-max3(X,Y,Z,X) :- X>Y, X>Z, !.
-max3(_,Y,Z,Y) :- Y>Z, !.
-max3(_,_,Z,Z).
-
-%сумма цифр(рекурсия вверх)
-
-sum_cifr(0,0):- !.
-sum_cifr(N,S) :- Cifr is N mod 10, N1 is N div 10, sum_cifr(N1, S1), S is S1 + Cifr.
-
-%сумма цифр(рекурсия вниз)
-
-sum_cifr_down(N,S) :- sum_cifr_down(N,0,S).
-sum_cifr_down(0,Sum,Sum) :- !.
-sum_cifr_down(N,Cur_sum,Sum) :- Cifr is N mod 10, N1 is N div 10, New_cur_sum is Cur_sum + Cifr,
-				sum_cifr_down(N1,New_cur_sum,Sum).
 
 
-%списки(они же списки Черча)
 
-write_list([]) :- !.
-write_list([H|T]) :- write(H), nl, write_list(T).
 
-summ_spisok([H|T],S) :- summ_spisok([H|T],0,S).
-summ_spisok([],Sum, Sum) :- !.
-summ_spisok([H|T],Cur_sum, Sum) :- New_cur_sum is Cur_sum + H, summ_spisok(T, New_cur_sum, Sum).
 
-%вложенные предикаты со списками. Базовые:
-%append
-%member - проверяет есть ли эл в списке.
-%nth0
-%reverse
+
+
+
+
 
